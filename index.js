53278be2976c590c97d3cf1c0b5abe21956f877b
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-02-11 17:11:41 +0600

    seond

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-var cors = require('cors')
-const app = express()
-const port = process.env.PORT || 4000;
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
 const news = require('./data/news.json');
 
 app.use(cors())
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send(news)
